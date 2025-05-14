dd33fb3abef9aff1b2787580c34adc2a7e0cbdef
muringo <muringo1017@gmail.com>
2025-05-14 23:19:51 +0900

    1부터 n까지의 m의 배수합
    채점 완료

---
+++ 코딩테스트인강/1부터_n까지의_m의_배수합/1부터_n까지의_m의_배수합.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+/*
+접근
+`1` 부터 `n`까지 `m` `배수`의 `합`
+
+1. n = 8
+   1~8, 1 2 3 4 5 6 7 8
+2. m = 2
+   2의 배수, 2 4 6 8 ....
+3. 배수의 합
+   2
+   2 + 4
+   (2 + 4) + 6
+   ((2 + 4) + 6) + 8 = 20
+
+*/
+
+int main()
+{
+    int n, m, i, sum = 0;
+    std::cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % m == 0) // i가 m의 배수라면
+        {
+            sum = sum + i; // 누적합
+        }
+
+    }//end of for
+
+    std::cout << sum;
+
+    return 0;
+}
